d0a8afa99cb1cfba3799e72e952780a6ff476b7c
poliyka <jkny3586@gmail.com>
2022-11-10 16:18:07 +0800

    feat(model): setup log model

--- backend/log/admin.py
+++ backend/log/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Log
 
 # Register your models here.
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    # lsit display all fields exclude updated_at
+    list_display = ("user", "method", "api", "action", "level", "message", "created_at")
+    list_filter = ("action", "level", "created_at")
+    search_fields = ("user", "class_", "api", "message")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
--- backend/log/models.py
+++ backend/log/models.py
@@ -1,3 +1,34 @@
+from base.models import BaseTimeModel
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Log(BaseTimeModel):
+    """Log model"""
+
+    class Level(models.TextChoices):
+        DEBUG = "debug", _("Debug")
+        INFO = "info", _("Info")
+        WARNING = "warning", _("Warning")
+        ERROR = "error", _("Error")
+        CRITICAL = "critical", _("Critical")
+
+    class Action(models.TextChoices):
+        SYSTEM = "system", _("System")
+        LOGIN = "login", _("Login")
+        LOGOUT = "logout", _("Logout")
+
+    user = models.CharField("使用者", max_length=32, blank=True, null=True)
+    method = models.CharField(
+        "方法名稱", max_length=32, help_text="func or class name like 'Login.as_view()'"
+    )
+    api = models.CharField(
+        "Api路徑", max_length=32, blank=True, null=True, help_text="api path like '/api/v1/xxx'"
+    )
+    action = models.CharField("執行操作", max_length=16, choices=Action.choices, default=Action.SYSTEM)
+    level = models.CharField("等級", max_length=8, choices=Level.choices, default=Level.INFO)
+    message = models.TextField("訊息", blank=True, null=True)
+
+    def __str__(self) -> str:
+        """String for representing the Model object."""
+        return f"[{self.level}]{self.message}"
