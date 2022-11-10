791360d2636205cf7bac639374b37e01dfdae56e
poliyka <jkny3586@gmail.com>
2022-11-10 16:20:01 +0800

    clean up model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,7 +1,5 @@
-import django.utils.timezone as timezone
-from django.contrib.auth.models import Group, Permission, User
+from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Sum
 
 
 class BaseTimeModel(models.Model):
@@ -33,12 +31,8 @@ class Profile(BaseTimeModel):
     # information
     name = models.CharField("姓名", max_length=32, blank=True, null=True)
     birth = models.DateField("生日", blank=True, null=True)
-    gender = models.CharField(
-        "性別", max_length=16, choices=Gender.choices, blank=True, null=True
-    )
-    identity = models.CharField(
-        "身分證字號", max_length=16, blank=True, null=True, unique=True
-    )
+    gender = models.CharField("性別", max_length=16, choices=Gender.choices, blank=True, null=True)
+    identity = models.CharField("身分證字號", max_length=16, blank=True, null=True, unique=True)
     tel = models.CharField("住家電話", max_length=16, blank=True, null=True)
     mobile = models.CharField("行動電話", max_length=32, blank=True, null=True)
     address = models.CharField("聯絡地址", max_length=256, blank=True, null=True)
