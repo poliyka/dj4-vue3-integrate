1ea7d29ff448c460b58b3a05de5c6e19074c4150
poliyka <jkny3586@gmail.com>
2022-11-24 11:23:24 +0800

    feat(table): fix scrollbar and add vxetable

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -47,7 +47,9 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <q-page padding>
+        <router-view />
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
