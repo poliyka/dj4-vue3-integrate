e46e2e0c22c0c826e24016248f2229d18f5a1cdd
poliyka <jkny3586@gmail.com>
2023-06-10 21:09:37 +0800

    update nginx

--- backend/django_base/settings/prod.py
+++ backend/django_base/settings/prod.py
@@ -2,15 +2,15 @@ from .base import *
 
 DEBUG = False
 
-# DATABASES = {
-#     "default": {
-#         # "ENGINE": "django.db.backends.postgresql",
-#         "ENGINE": "psqlextra.backend",
-#         "OPTIONS": {"options": f"-c search_path={env('POSTGRES_SCHEMA')},public"},
-#         "NAME": env("POSTGRES_DB"),
-#         "USER": env("POSTGRES_USER"),
-#         "PASSWORD": env("POSTGRES_PASSWORD"),
-#         "HOST": env("POSTGRES_HOST"),
-#         "PORT": env("POSTGRES_PORT"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        # "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "psqlextra.backend",
+        "OPTIONS": {"options": f"-c search_path={env('POSTGRES_SCHEMA')},public"},
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
